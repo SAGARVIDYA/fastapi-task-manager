ec5d4fbb80f1e9d63c76bba320dfc2db4936a0a4
Vidya Sagar.S <vsagar71004@gmail.com>
2026-04-19 16:27:26 +0530

    sqlite deploy

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -2,19 +2,12 @@ from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+SECRET_KEY = "mysecretkey"
 
+ALGORITHM = "HS256"
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-ALGORITHM = os.getenv("ALGORITHM")
-
-ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-)
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 pwd_context = CryptContext(
@@ -31,13 +24,17 @@ def hash_password(password: str):
 
 # verify password
 def verify_password(
+
         plain_password,
         hashed_password
+
 ):
 
     return pwd_context.verify(
+
         plain_password,
         hashed_password
+
     )
 
 
@@ -47,31 +44,35 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(
+
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+
     )
 
-    to_encode.update(
-        {"exp": expire}
-    )
+    to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(
+
         to_encode,
         SECRET_KEY,
         algorithm=ALGORITHM
+
     )
 
     return encoded_jwt
 
 
-# verify token
+# verify token  ⭐ IMPORTANT
 def verify_token(token: str):
 
     try:
 
         payload = jwt.decode(
+
             token,
             SECRET_KEY,
             algorithms=[ALGORITHM]
+
         )
 
         return payload
